44e95ae0bfe25a641d87bf8f6c6ebe2a1f9d0f31
Kiran <110820842+KiranMittapalle@users.noreply.github.com>
2022-08-10 14:59:41 +0300

    Update gendata.m

--- gendata.m
+++ gendata.m
@@ -9,16 +9,10 @@ Yte = labels(testind);
 
 Xtr = []; Ytr = []; 
 
-h = waitbar(0,'Please wait...');
-
 for i = 1:numel(trn)
 
     Xtr = horzcat(Xtr,trn{i});
     Ytr = vertcat(Ytr, replab(ytrn(i),size(trn{i},2)));
-    
-    waitbar(i/numel(trn), h, sprintf('Generating Train/Test data: %d %%', floor(i/numel(trn)*100)));
+
 end
-
-close(h)
-
 end
